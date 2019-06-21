0d7412ace11fb42e4059af55fee2627d5b1e0373
abhimanyu-jain <47700785+abhimanyu-jain@users.noreply.github.com>
2019-06-21 17:21:40 -0400

    Aerobee 350
    Aerobee 350
    
    Payload: 227 kg
    Maximum flight altitude: 270 mi (450 km)[11]
    Liftoff Thrust: 217 kN
    Total Mass: 3,839 kg
    Core Diameter: 0.56 m
    Total Length: 15.90 m
    Span: 2.30 m
    
    
    Aerobee 350 specifications[citation needed]
    
    Booster:
    Loaded weight                 1305 lb (593 kg)
    Thrust                        48,700 lbf (217,000 N)
    Duration                      3.5 s
    Impulse                       170,000 lbf s (756,000 N s)
    NAR designation               T 220,000
    
    Sustainer:
    Loaded weight less payload    6642LB (3,019 kg)
    Propellant weight             4,335 lb (1,970 kg)
    Payload weight                150-500 lb (68–227 kg)
    Thrust                        4X4100 lbf (18,000 N)
    Duration                      52.7 s
    Impulse                       864,000 lbf s (3,850,000 N s)
    NAR designation               4 X T 18,000

--- peak_altitude_calculation.m
+++ peak_altitude_calculation.m
@@ -1,27 +1,64 @@
+clear;
+clc;
+
 % Equation explanation : http://www.rocketmime.com/rockets/qref.html
 
-m = 1.849% rocket mass in kg
+mr = 227 % empty (no motor) mass of your rocket
+me = 593 % loaded mass of your motor
+mp = % mass of the propellant
 g = 9.81 % acceleration of gravity = 9.81 m/s2
-A = pi*1/2*(0.0678434)*(0.0678434) % rocket cross-sectional area in m2
+A = pi*1/4*(0.56)*(0.56) % rocket cross-sectional area in m2
 Cd = 0.75 % drag coefficient = 0.75 for average rocket
 r = 1.22 % air density = 1.22 kg/m3
-t = 0.326 % motor burn time in seconds (NOTE: little t)
-T = 982 % motor thrust in Newtons (NOTE: big T) max : 1710
-I = 320 % motor impulse in Newton-seconds
+t = 0 % motor burn time in seconds (NOTE: little t)
+T = 217000 + 72000 % motor thrust in Newtons (NOTE: big T) max : 1710
+I = 756000 + 3850000 % motor impulse in Newton-seconds
 v = 0% burnout velocity in m/s
 y1 = 0% altitude at burnout
 yc = 0% coasting distance
 % Note that the peak altitude is y1 + yc
 ta = 0% coasting time => delay time for motor
-    
-k = 1/2*r*Cd*A
-q = sqrt((T - m*g)/k)
-x = 2*k*q/m
-t = I/T
-v = q*(1 - exp(-x*t))/(1 + exp(-x*t))
-y1 = (-m/(2*k))*log((T - m*g - k*v*v)/(T - m*g))
-yc = (m/(2*k))*log((m*g + k*v*v)/(m*g))
-peak_altitude = (y1 + yc)*3.28084
-sprintf('max altitude = %f feet', peak_altitude)
 
+[y1, yc, v] = altitdue_1st_stage(me, mr, mp, A, r, T, I, Cd, g)
+
+mr = 227 % empty (no motor) mass of your rocket
+me = 3019 % loaded mass of your motor
+mp = 1970 % mass of the propellant
+g = 9.81 % acceleration of gravity = 9.81 m/s2
+A = pi*1/4*(0.56)*(0.56) % rocket cross-sectional area in m2
+Cd = 0.75 % drag coefficient = 0.75 for average rocket
+r = 1.22 % air density = 1.22 kg/m3
+T = 72000 % motor thrust in Newtons (NOTE: big T) max : 1710
+I = 3850000 % motor impulse in Newton-seconds
+
+[y2, yd] = altitude_upper_stage(me, mr, mp, A, r, T, I, Cd, g, v)
+
+peak_altitude = (y1 + y2 + yd)
+sprintf('max altitude = %f km', peak_altitude/1000)
+sprintf('max altitude = %f feet', peak_altitude*3.28084)
 % Result : 'max altitude = 1722.104332 feet'
+
+function [y1, yc, v] = altitdue_1st_stage(me, mr, mp, A, r, T, I, Cd, g)
+    m_avg = mr +me -mp/2
+    m_coast = mr + me - mp
+    k = 1/2*r*Cd*A
+    q = sqrt((T - m_avg*g)/k)
+    x = 2*k*q/m_avg
+    t = I/T
+    v = q*(1 - exp(-x*t))/(1 + exp(-x*t))
+    y1 = (-m_avg/(2*k))*log((T - m_avg*g - k*v*v)/(T - m_avg*g))
+    yc = (m_coast/(2*k))*log((m_coast*g + k*v*v)/(m_coast*g))
+end
+
+function [y1, yc] = altitude_upper_stage(me, mr, mp, A, r, T, I, Cd, g, v0)
+    m_avg = mr +me -mp/2
+    m_coast = mr + me - mp
+    k = 1/2*r*Cd*A
+    q = sqrt((T - m_avg*g)/k)
+    x = 2*k*q/m_avg
+    t = I/T
+    s = (q + v0)/(q - v0)
+    v = q*(s - exp(-x*t))/(s + exp(-x*t))
+    y1 = (-m_avg/(2*k))*log((T - m_avg*g - k*v*v)/(T - m_avg*g - k*v0*v0))
+    yc = (m_coast/(2*k))*log((m_coast*g + k*v*v)/(m_coast*g))
+end
